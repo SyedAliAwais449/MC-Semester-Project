ad6ca7ba1dfe7796b8adbcd909975c2fe43b318a
SyedAliAwais449 <saliawaisd449@gmail.com>
2022-01-20 02:46:45 +0500

    App bar along with basic room added

--- app/src/main/java/com/example/mcsemesterproject/Home.java
+++ app/src/main/java/com/example/mcsemesterproject/Home.java
@@ -1,14 +1,46 @@
 package com.example.mcsemesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Home extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+//        toolbar= findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+//                Intent intent= new Intent(MainActivity.this, MainActivity3.class);
+//                startActivity(intent);
+                break;
+            case R.id.stays:
+//                Intent intent2= new Intent(MainActivity.this, MainActivity2.class);
+//                startActivity(intent2);
+                break;
+            default:
+// If we got here, the user's action was not recognized.
+// Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+        return false;
     }
 }
