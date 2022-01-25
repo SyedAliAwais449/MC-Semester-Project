3fec2243178d838c796f1c390b1f35f8a2b46484
SaadHassan117 <91969168+SaadHassan117@users.noreply.github.com>
2022-01-25 12:43:22 -0700

    Revert " signup"
    This reverts commit 775a80d06d3acfc2e610dcb7ded36de5c5f94b74.

--- app/src/main/java/com/example/mcsemesterproject/Login.java
+++ app/src/main/java/com/example/mcsemesterproject/Login.java
@@ -2,26 +2,13 @@ package com.example.mcsemesterproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-   Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        btn=findViewById(R.id.signup);
-        btn.setOnClickListener(new View.OnClickListener() {
-            Intent inte;
-            @Override
-
-            public void onClick(View v) {
-                inte =new Intent (Login.this, SignUp.class);
-                startActivity(inte);
-            }
-        });
     }
 }
